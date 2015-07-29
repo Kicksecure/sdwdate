9fd86a920950ea3f5bd5903710893406c658ba7f
troubadoour <trobador@riseup.net>
2015-07-29 20:36:43 +0000

    config reads multi-line entries, append random url to pool

--- usr/lib/python2.7/dist-packages/sdwdate/config.py
+++ usr/lib/python2.7/dist-packages/sdwdate/config.py
@@ -1,9 +1,45 @@
 #!/usr/bin/env python
 
-import os, sys
+import os
 import glob
-import time
 import re
+import random
+
+def sort_pool(pool):
+    ## Check number of multi-line pool.
+    double_quotes = 0
+    for i in range(len(pool)):
+        if pool[i] == ('"'):
+            double_quotes = double_quotes + 1
+    number_of_pool_multi =  double_quotes / 2
+
+    ## Dynamically create multi-line lists.
+    multi_list = [[] for i in range(number_of_pool_multi)]
+
+    ## Sort...
+    multi_line = False
+    multi_index = 0
+    pool_single = []
+    for i in range(len(pool)):
+        if multi_line and pool[i] == '"':
+            multi_line = False
+            multi_index = multi_index + 1
+        elif multi_line:
+            url = re.search(r'"(.*)#', pool[i])
+            multi_list[multi_index].append(url.group(1))
+        elif pool[i] == '"':
+            multi_line = True
+        elif pool[i].startswith('"'):
+            url = re.search(r'"(.*)#', pool[i])
+            pool_single.append(url.group(1))
+
+    ## Pick a random url in each multi-line pool,
+    ## append it to single url pool.
+    for i in range(number_of_pool_multi):
+        single_url = multi_list[i][random.sample(range(len(multi_list[i])), 1)[0]]
+        pool_single.append(single_url)
+
+    return(pool_single)
 
 def read_pools():
     SDWDATE_POOL_ONE = False
@@ -13,6 +49,10 @@ def read_pools():
     pool_one = []
     pool_two = []
     pool_three = []
+
+    pool_one_sorted = []
+    pool_two_sorted = []
+    pool_three_sorted = []
 
     if os.path.exists('/etc/sdwdate-python.d/'):
         files = sorted(glob.glob('/etc/sdwdate-python.d/*'))
@@ -37,17 +77,14 @@ def read_pools():
                                 SDWDATE_POOL_TWO = False
                                 SDWDATE_POOL_THREE = True
 
-                            elif SDWDATE_POOL_ONE and line.startswith('"'):
-                                url = re.search(r'"(.*)#', line)
-                                pool_one.append(url.group(1))
+                            elif SDWDATE_POOL_ONE and not line.startswith('##'):
+                                pool_one.append(line)
 
-                            elif SDWDATE_POOL_TWO and line.startswith('"'):
-                                url = re.search(r'"(.*)#', line)
-                                pool_two.append(url.group(1))
+                            elif SDWDATE_POOL_TWO and not line.startswith('##'):
+                                pool_two.append(line)
 
-                            elif SDWDATE_POOL_THREE and line.startswith('"'):
-                                url = re.search(r'"(.*)#', line)
-                                pool_three.append(url.group(1))
+                            elif SDWDATE_POOL_THREE and not line.startswith('##'):
+                                pool_three.append(line)
 
             if not conf_found:
                 print('No valid file found in user configuration folder "/etc/sdwdate.d".')
@@ -58,12 +95,15 @@ def read_pools():
     else:
         print('User configuration folder "/etc/sdwdate.d" does not exist.')
 
-    ## Remove duplicates.
-    pool_one = list(set(pool_one))
-    pool_two = list(set(pool_two))
-    pool_three = list(set(pool_three))
+    pool_one_sorted = sort_pool(pool_one)
+    pool_two_sorted = sort_pool(pool_two)
+    pool_three_sorted = sort_pool(pool_three)
 
-    return (pool_one, pool_two, pool_three)
+    print pool_one_sorted
+    print pool_two_sorted
+    print pool_three_sorted
+
+    return(pool_one_sorted,  pool_two_sorted, pool_three_sorted)
 
 if __name__ == "__main__":
     read_pools()
