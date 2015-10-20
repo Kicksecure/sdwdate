236627f994938080e24995118cfe4572e47b0c72
troubadoour <trobador@riseup.net>
2015-10-20 20:43:57 +0000

    config.py -> production/test modes

--- usr/lib/python2.7/dist-packages/sdwdate/config.py
+++ usr/lib/python2.7/dist-packages/sdwdate/config.py
@@ -5,7 +5,7 @@ import glob
 import re
 import random
 
-def sort_pool(pool):
+def sort_pool(pool, mode):
     ## Check number of multi-line pool.
     number_of_pool_multi = 0
     for i in range(len(pool)):
@@ -30,10 +30,16 @@ def sort_pool(pool):
         elif multi_line and pool[i].startswith('"'):
             url = re.search(r'"(.*)#', pool[i])
             if url != None:
-                multi_list_url[multi_index].append(url.group(1))
+                if mode == 'production':
+                    multi_list_url[multi_index].append(url.group(1))
+                elif mode == 'test':
+                    pool_single_url.append(url.group(1))
             comment = re.search(r'#(.*)"', pool[i])
             if comment != None:
-                multi_list_comment[multi_index].append(comment.group(1))
+                if mode == 'production':
+                    multi_list_comment[multi_index].append(comment.group(1))
+                elif mode == 'test':
+                    pool_single_comment.append(comment.group(1))
 
         elif pool[i] == '[':
             multi_line = True
@@ -49,15 +55,16 @@ def sort_pool(pool):
     ## Pick a random url in each multi-line pool,
     ## append it to single url pool.
     for i in range(number_of_pool_multi):
-        single_ulr_index = random.sample(range(len(multi_list_url[i])), 1)[0]
-        single_url = multi_list_url[i][single_ulr_index]
-        single_comment = multi_list_comment[i][single_ulr_index]
-        pool_single_url.append(single_url)
-        pool_single_comment.append(single_comment)
+        if mode == 'production':
+            single_ulr_index = random.sample(range(len(multi_list_url[i])), 1)[0]
+            single_url = multi_list_url[i][single_ulr_index]
+            single_comment = multi_list_comment[i][single_ulr_index]
+            pool_single_url.append(single_url)
+            pool_single_comment.append(single_comment)
 
     return(pool_single_url, pool_single_comment)
 
-def read_pools(pool):
+def read_pools(pool, mode):
     SDWDATE_POOL_ONE = False
     SDWDATE_POOL_TWO = False
     SDWDATE_POOL_THREE = False
@@ -111,9 +118,9 @@ def read_pools(pool):
     else:
         print('User configuration folder "/etc/sdwdate.d" does not exist.')
 
-    pool_one_url, pool_one_comment = sort_pool(pool_one)
-    pool_two_url , pool_two_comment = sort_pool(pool_two)
-    pool_three_url, pool_three_comment = sort_pool(pool_three)
+    pool_one_url, pool_one_comment = sort_pool(pool_one, mode)
+    pool_two_url , pool_two_comment = sort_pool(pool_two, mode)
+    pool_three_url, pool_three_comment = sort_pool(pool_three, mode)
 
     pool_url = [pool_one_url, pool_two_url, pool_three_url]
     pool_comment = [pool_one_comment, pool_two_comment, pool_three_comment]
