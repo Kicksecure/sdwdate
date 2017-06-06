ca3ffa3d8559bcfaf35f5d294bd933cea20ecaef
JasonJAyalaP <JasonJAyalaP+Github@Gmail.com>
2017-06-06 11:41:03 -0500

    Replacement of old sclockadj

--- usr/lib/sdwdate/sclockadj.c
+++ usr/lib/sdwdate/sclockadj.c
@@ -14,7 +14,7 @@ See the file COPYING for copying conditions.
 void change_time_by_nanoseconds(long long add_ns)
 {
     struct timespec tps; /* tv_sec; tv_nsec */
-    if (clock_gettime(0, &tps) == -1) /* get current time in seconds since epoch + nanoseconds offset */
+    if (clock_gettime(CLOCK_REALTIME, &tps) == -1) /* get current time in seconds since epoch + nanoseconds offset */
     {
         perror("Failed to get current time!");
         exit(EXIT_FAILURE);
@@ -31,7 +31,7 @@ void change_time_by_nanoseconds(long long add_ns)
     tps.tv_sec = new_s;
     tps.tv_nsec = new_ns;
     /* set time with new values */
-    if (clock_settime(0, &tps) == -1)
+    if (clock_settime(CLOCK_REALTIME, &tps) == -1)
     {
         perror("Failed to change clock!");
         exit(EXIT_FAILURE);
@@ -41,7 +41,7 @@ void change_time_by_nanoseconds(long long add_ns)
 /* intended to be used only by sdwdate with sane inputs */
 int main(int argc, char *argv[])
 {
-    if (argc < 1) exit(EXIT_FAILURE);
+    if (argc < 2) exit(EXIT_FAILURE);
     long long ns_time_change = atoll(argv[1]); /* convert argv string into long long */
     if (ns_time_change == 0) 
     {
@@ -51,15 +51,16 @@ int main(int argc, char *argv[])
     
     /* since nanosecond jump is fixed, we can count the number of complete jumps. */
     /* llabs for negative numbers */
-    long long number_of_full_jumps = llabs(ns_time_change) / 5000000;  /* times we'll move clock by 5,000,000 ns at a time */
-    long long last_jump_nanoseconds = llabs(ns_time_change) % 5000000; /* then add remaining < 5,000,000 ns */
+    static int const full_jump = 5000000;
+    long long number_of_full_jumps = llabs(ns_time_change) / full_jump;  /* times we'll move clock by 5,000,000 ns at a time */
+    long long last_jump_nanoseconds = llabs(ns_time_change) % full_jump; /* then add remaining < 5,000,000 ns */
 
     if (ns_time_change > 0) /* positive nanosecond change */
     {
         for (unsigned i = 0; i < number_of_full_jumps; ++i) 
         {
             sleep(1);  /* a 1 second wait imitates ntpdate */
-            change_time_by_nanoseconds(5000000); /* 5,000,000 ns imitates ntpdate */
+            change_time_by_nanoseconds(full_jump); /* 5,000,000 ns imitates ntpdate */
         }
         sleep(1);
         change_time_by_nanoseconds(last_jump_nanoseconds);
@@ -69,7 +70,7 @@ int main(int argc, char *argv[])
         for (unsigned i = 0; i < number_of_full_jumps; ++i) 
         {
             sleep(1);
-            change_time_by_nanoseconds(-5000000);
+            change_time_by_nanoseconds(-full_jump);
         }
         sleep(1);
         change_time_by_nanoseconds(-last_jump_nanoseconds); /* negative of absolute value imitates Euclidean modulo */
