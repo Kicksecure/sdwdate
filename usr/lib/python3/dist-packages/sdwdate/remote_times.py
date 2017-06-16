5f0ca92f7a0bb6d2d15fbba7644278c010e450b0
JasonJAyalaP <JasonJAyalaP+Github@Gmail.com>
2017-06-16 18:10:47 -0500

    handle bad responses

--- usr/lib/python3/dist-packages/sdwdate/remote_times.py
+++ usr/lib/python3/dist-packages/sdwdate/remote_times.py
@@ -56,9 +56,10 @@ def get_time_from_servers(remotes, ip_address, port_number):
     for i in range(len(threads)):
         if threads[i].poll() is not None:
             urls.append(remotes[i])
-            timestamp = threads[i].stdout.read().strip()
-            time_format = datetime.datetime.utcfromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
-            unix_times.append(time_format + " / " + timestamp.decode("utf-8"))
+            timestamp = threads[i].stdout.read().strip().decode("utf-8")
+            if timestamp.isdecimal():
+                timestamp = datetime.datetime.utcfromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S') + " / " + timestamp
+            unix_times.append(timestamp)
         else:
             urls.append(remotes[i])
             unix_times.append('Timeout')
