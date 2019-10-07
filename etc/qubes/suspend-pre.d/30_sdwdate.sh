e455041ba80258427c9c8ff43ee34294f62ec7dd
Patrick Schleizer <adrelanos@riseup.net>
2019-10-07 05:53:40 +0000

    code documentation

--- etc/qubes/suspend-pre.d/30_sdwdate.sh
+++ etc/qubes/suspend-pre.d/30_sdwdate.sh
@@ -3,6 +3,13 @@
 ## Copyright (C) 2017 - 2018 ENCRYPTED SUPPORT LP <adrelanos@riseup.net>
 ## See the file COPYING for copying conditions.
 
+#### meta start
+#### project Whonix
+#### category networking and time
+#### description hook to run <code>/usr/lib/sdwdate/suspend-pre</code>
+## in Qubes-Whonix.
+#### meta end
+
 set -e
 
 /usr/lib/sdwdate/suspend-pre
