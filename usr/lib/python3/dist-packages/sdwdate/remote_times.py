17e985c00909c7d8b674195d8e66746455a4e167
Patrick Schleizer <adrelanos@riseup.net>
2021-01-19 12:01:33 -0500

    correctly calculate took time

--- usr/lib/python3/dist-packages/sdwdate/remote_times.py
+++ usr/lib/python3/dist-packages/sdwdate/remote_times.py
@@ -10,109 +10,125 @@ import shlex
 import time
 from gevent.subprocess import Popen, PIPE
 
-def get_time_from_servers(remotes, ip_address, port_number):
-    threads = []
+from subprocess import Popen
+from threading import Thread
+import time
+
+start_unixtime = [None] * 5
+took_time = [None] * 5
+
+class Process(Popen):
+    def register_callback(self, callback, *args, **kwargs):
+        Thread(target=self.__bootstrap, args=(callback, args, kwargs)).start()
+    def __bootstrap(self, callback, args, kwargs):
+        self.wait()
+        callback(*args, **kwargs)
+
+def get_time_from_servers_callback(handle, i):
+    #print("get_time_from_servers_callback i: " + str(i))
+    #print("get_time_from_servers_callback exit code: " + str(handle.returncode))
+    end_unixtime = time.time()
+    took_time[i] = end_unixtime - start_unixtime[i]
+    #print("get_time_from_servers_callback took_time: " + str(took_time[i]))
+
+def get_time_from_servers(list_of_remote_servers, proxy_ip_address, proxy_port_number):
     urls = []
     stdout_list = []
     stderr_list = []
     took_time_list = []
-    seconds = 50
+    timeout_seconds = 50
     do_exit = False
     remote_port = "80"
     ## TODO: set to "false"
     url_to_unixtime_debug = "true"
 
     ### Clear lists.
-    del threads[:]
     del urls[:]
     del stdout_list[:]
     del stderr_list[:]
     del took_time_list[:]
 
-    for i in range(len(remotes)):
-      url_to_unixtime_command = "url_to_unixtime" + " " + ip_address + " " + port_number + " " + remotes[i] + " " + remote_port + " " + url_to_unixtime_debug
+    handle = [None] * 5
+
+    for i in range(len(list_of_remote_servers)):
+      url_to_unixtime_command = "url_to_unixtime" + " " + proxy_ip_address + " " + proxy_port_number + " " + list_of_remote_servers[i] + " " + remote_port + " " + url_to_unixtime_debug
 
       print("remote_times.py: url_to_unixtime_command:", str(url_to_unixtime_command))
 
       ## Avoid Popen shell=True.
       url_to_unixtime_command = shlex.split(url_to_unixtime_command)
 
-      start_unixtime = time.time()
-      threads.append(Popen(url_to_unixtime_command, stdout=PIPE, stderr=PIPE))
+      start_unixtime[i] = time.time()
 
-    try:
-       gevent.wait(timeout=seconds)
-    except KeyboardInterrupt:
-       do_exit = True
-       print("remote_times.py: KeyboardInterrupt received.")
-    except SystemExit:
-       do_exit = True
-       print("remote_times.py: sigterm received.")
-    except:
-       print("remote_times.py: unexpected error:", sys.exc_info()[0])
-       pass
+      handle[i] = Process(url_to_unixtime_command, stdout=PIPE, stderr=PIPE)
+      handle[i].register_callback(get_time_from_servers_callback, handle[i], i)
+
+    for i in range(len(list_of_remote_servers)):
+      try:
+         handle[i].wait(timeout=timeout_seconds)
+         print("remote_times.py: wait_ok i:", str(i))
+      except KeyboardInterrupt:
+         do_exit = True
+         print("remote_times.py: KeyboardInterrupt received.")
+      except SystemExit:
+         do_exit = True
+         print("remote_times.py: SystemExit received.")
+      except:
+         print("remote_times.py: wait sys.exc_info:", sys.exc_info()[0])
+         handle[i].kill()
+         ## Results in invoking get_time_from_servers_callback.
+         handle[i].wait()
 
     if do_exit == True:
-       for i in range(len(threads)):
+       for i in range(len(list_of_remote_servers)):
            try:
-               threads[i].terminate()
+               handle[i].kill()
            except:
                pass
-       urls.append(remotes[i])
-       stdout_list.append('sigterm')
-       stderr_list.append('sigterm')
-       return urls, stdout_list, stderr_list
+           urls.append(list_of_remote_servers[i])
+           stdout_list.append('sigterm')
+           stderr_list.append('sigterm')
+           took_time_list.append(0)
+       return urls, stdout_list, stderr_list, took_time_list
 
-    for i in range(len(threads)):
-        if threads[i].poll() is not None:
-            end_unixtime = time.time()
-            took_time = end_unixtime - start_unixtime
-            took_time_list.append(took_time)
-            urls.append(remotes[i])
-            returncode = threads[i].returncode
+    for i in range(len(list_of_remote_servers)):
+       took_time_list.append(took_time[i])
+       urls.append(list_of_remote_servers[i])
+       returncode = handle[i].returncode
 
-            ## bytes
-            stdout = threads[i].stdout.read()
-            stderr = threads[i].stderr.read()
+       ## bytes
+       stdout = handle[i].stdout.read()
+       stderr = handle[i].stderr.read()
 
-            #print("remote_times.py: stdout: " + str(stdout))
+       #print("remote_times.py: i: " + str(i))
+       #print("remote_times.py: stdout: " + str(stdout))
+       #print("remote_times.py: took_time[i]: " + str(took_time[i]))
+       #print("remote_times.py: returncode: " + str(returncode))
 
-            if not returncode == 0:
-               ## str
-               stdout_to_append = ""
-               stdout_to_append += str(stdout)
-               stdout_to_append += " | non-zero exit code: " + str(returncode)
-               stdout_list.append(stdout_to_append)
-               stderr_to_append = ""
-               stdout_to_append += str(stderr)
-               stderr_to_append += " | non-zero exit code: " + str(returncode)
-               stderr_list.append(stderr_to_append)
-            else:
-               stdout_list.append(stdout)
-               stderr_list.append(stderr)
-        else:
-            urls.append(remotes[i])
-            stdout_list.append('Timeout')
-            stderr_list.append('Timeout')
-            end_unixtime = time.time()
-            took_time = end_unixtime - start_unixtime
-            took_time_list.append(took_time)
-            took_time_list.append(took_time)
-        try:
-           threads[i].terminate()
-        except:
-           pass
+       if not returncode == 0:
+          ## str
+          stdout_to_append = ""
+          stdout_to_append += str(stdout)
+          stdout_to_append += " | non-zero exit code: " + str(returncode)
+          stdout_list.append(stdout_to_append)
+          stderr_to_append = ""
+          stdout_to_append += str(stderr)
+          stderr_to_append += " | non-zero exit code: " + str(returncode)
+          stderr_list.append(stderr_to_append)
+       else:
+          stdout_list.append(stdout)
+          stderr_list.append(stderr)
 
-    print("urls:")
+    print("remote_times.py: urls:")
     print(str(urls))
-    print("stdout_list:")
+    print("remote_times.py: stdout_list:")
     print(str(stdout_list))
-    print("stderr_list:")
+    print("remote_times.py: stderr_list:")
     print(str(stderr_list))
-    print("took_time_list:")
+    print("remote_times.py: took_time_list:")
     print(str(took_time_list))
 
     return urls, stdout_list, stderr_list, took_time_list
 
 if __name__ == "__main__":
-    get_time_from_servers(remotes)
+    get_time_from_servers(list_of_remote_servers)
